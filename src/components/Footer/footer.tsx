8f52b4437ed7880ccc57577c089be302ec92a21d
Katy Rosli <96566968+KatyRosli@users.noreply.github.com>
2022-06-26 16:39:15 +0200

    edited footer component & created footer css

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,27 +1,28 @@
 import React from "react";
-
-import Footer from 'react-bootstrap/Footer';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
 import ReactDOM from 'react-dom';
+import "./footer.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faFacebookF, faInstagram, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 library.add(faFacebookF, faInstagram, faTwitter, faYoutube);
 
+
 const Footer = () => {
     return (
         <Container>
-            <Footer className="Footer">
-            <Nav.Link href="#instagram"><FontAwesomeIcon icon={faInstagram} /></Nav.Link>
-            <Nav.Link href="#twitter"><FontAwesomeIcon icon={faTwitter} /></Nav.Link>
-            <Nav.Link href="#facebook"><FontAwesomeIcon icon={faFacebookF} /></Nav.Link>
-            <Nav.Link href="#youtube"><FontAwesomeIcon icon={faYoutube} /></Nav.Link>
-            <div> © 2022 MANGO All rights reserved </div>
-            <Container>
-            <Nav.Link href="#PolicyAndCookies">Privacy Policy and Cookies</Nav.Link>
-            <div> | </div> 
-            <Nav.Link href="#TermsAndConditions">Terms and Conditions</Nav.Link>
-            </Container>
-            </Footer>
+        <Nav className="justify-content-center" activeKey="/home">
+        <Nav.Item><Nav.Link href="#instagram"><FontAwesomeIcon icon={faInstagram} /></Nav.Link></Nav.Item>
+        <Nav.Item><Nav.Link href="#twitter"><FontAwesomeIcon icon={faTwitter} /></Nav.Link></Nav.Item>    
+        <Nav.Item><Nav.Link href="#facebook"><FontAwesomeIcon icon={faFacebookF} /></Nav.Link></Nav.Item>   
+        <Nav.Item><Nav.Link href="#youtube"><FontAwesomeIcon icon={faYoutube} /></Nav.Link></Nav.Item> 
+        <Nav.Item>
+        <div className="copyrights"> © 2022 silverspoon All rights reserved </div>
+        <Nav.Link href="#PolicyAndCookies">Privacy Policy and Cookies |</Nav.Link>
+        <Nav.Link href="#TermsAndConditions">Terms and Conditions</Nav.Link>
+        </Nav.Item>
+        </Nav>
         </Container>
         )
 }
